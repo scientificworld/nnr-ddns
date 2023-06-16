77ad9bbd3afd2fd665ea47f830d729c26e337b56
scientificworld <scientificworld@126.com>
2023-06-16 23:52:03 +0800

    cache

--- nnrddnsd.sh
+++ nnrddnsd.sh
@@ -31,6 +31,7 @@ TOKEN="`cat "$CONFIG" | jq -r .token`"
 PROTOCOL="`cat "$CONFIG" | jq -r .protocol`"
 INTERFACE="`cat "$CONFIG" | jq .interface`"
 RULES="`cat "$CONFIG" | jq -r ".rules[]"`"
+CACHE="`cat "$CONFIG" | jq -r .cache`"
 
 OPTION=""
 if [[ "$PROTOCOL" = "4" ]] || [[ "$PROTOCOL" = "6" ]]
@@ -42,23 +43,32 @@ then
 	OPTION="$OPTION --interface `echo "$INTERFACE" | jq -r`"
 fi
 
+mkdir -p /var/nnr-ddns
+
 while true
 do
 	ip="`curl ip.sb -s $OPTION`"
 	info "current ip: $ip"
 	for rule in $RULES
 	do
-		data="`curl https://nnr.moe/api/rules/get -s -H "Content-Type: application/json" -H "Token: $TOKEN" -X POST -d "{\\\"rid\\\": \\\"$rule\\\"}"`"
-		if [[ "`echo "$data" | jq .status`" != "1" ]]
+		if [[ "$CACHE" = "true" ]] && [[ -f "/var/nnr-ddns/$rule" ]]
 		then
-			warning "unable to fetch rule $rule"
-			continue
+			remote="`cat "/var/nnr-ddns/$rule"`"
+		else
+			data="`curl https://nnr.moe/api/rules/get -s -H "Content-Type: application/json" -H "Token: $TOKEN" -X POST -d "{\\\"rid\\\": \\\"$rule\\\"}"`"
+			if [[ "`echo "$data" | jq .status`" != "1" ]]
+			then
+				warning "unable to fetch rule $rule"
+				continue
+			fi
+			remote="`echo "$data" | jq -r .data.remote`"
 		fi
-		if [[ "`echo "$data" | jq -r .data.remote`" = "$ip" ]]
+		if [[ "$remote" = "$ip" ]]
 		then
 			info "rule $rule is up to date"
 		else
 			curl https://nnr.moe/api/rules/edit -s -o /dev/null -H "Content-Type: application/json" -H "Token: $TOKEN" -X POST -d "{\"rid\": \"$rule\", \"remote\": \"$ip\", \"rport\": \"`echo "$data" | jq -r .data.rport`\", \"name\": \"`echo "$data" | jq -r .data.name`\", \"setting\": `echo "$data" | jq -r .data.setting`}"
+			echo "$ip" >/var/nnr-ddns/$rule
 			info "rule $rule is updated"
 		fi
 	done
